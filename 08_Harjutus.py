0a9e846ef4d847021df9f523c38c96d326df4951
datermination <alekseileonov15@gmail.com>
2025-10-25 13:00:31 +0300

    yes

--- 08_Harjutus.py
+++ 08_Harjutus.py
@@ -1,7 +1,36 @@
 # Harjutus 08
 
 
-sonastik = {'nimi': 'Margus', 'vanus': 25}
+telefonid={
+'Mari': '5957503',
+'Toomas': '5719979',
+'Kertu': '5201750',
+'Siim': '5580027',
+'Piret': '5960799',
+'Jaan': '5160415',
+'Lea': '5760164',
+'Mart': '5337951',
+'Anni': '5004818',
+'Tõnu': '5721873',
+'Kai': '5811884',
+'Rasmus': '5859489',
+'Eva': '5039393',
+'Oskar': '5635812',
+'Liina': '5696114',
+'Peeter': '5560756',
+'Sandra': '5257415',
+'Heiki': '5207248',
+'Kristi': '5703338',
+'Urmas': '5400549'
+}
 
-print (sonastik['nimi'])
-print (sonastik['nimi'])
+telefonid["Mario"]="45454554454"
+eemalda = telefonid.pop("Kristi")
+telefonid["Eva"] = "5555555555555555555"
+
+print(telefonid['Rasmus'])
+print(eemalda)
+print(telefonid['Eva'])
+
+for i in telefonid:
+    print(telefonid[i])
